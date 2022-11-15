1ff7e50625eed1e3191d92cdd278d9776bbe22a4
sergio <sergio.ortiz@tekschool.us>
2022-11-15 05:47:02 -0800

    merge branch Repo1

--- src/eclipseGits/gitPactice.java
+++ src/eclipseGits/gitPactice.java
@@ -5,7 +5,9 @@ public class gitPactice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a = 10;
+int b = 5;
 System.out.println(a);
+System.out.println(a / b);
 
 	}
 
